99e74d29101154d28ca115945ddf76a65b647c43
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2023-09-03 20:45:38 +0200

    Add custom config

--- benchmark_utils/config.py
+++ benchmark_utils/config.py
@@ -1,2 +1,2 @@
-DATA_PATH_IBC = "..." # path to IBC data
-MEMORY = "..."        # path to memory
+DATA_PATH_IBC =  "/data/parietal/store3/work/pbarbara/fmralignbench_data/ibc" # path to IBC data
+MEMORY = "/data/parietal/store3/work/pbarbara/tmp" # path to memory
