8ed7be7a45dbea6d739b4e5df7f12490faed1aa3
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2023-09-04 02:31:39 +0200

    Add forrest dataset

--- benchmark_utils/config.py
+++ benchmark_utils/config.py
@@ -2,5 +2,14 @@
 DATA_PATH_IBC = (
     "/data/parietal/store3/work/pbarbara/fmralignbench_data/ibc"
 )
+
+DATA_PATH_NEUROMOD = (
+    "/data/parietal/store3/work/pbarbara/fmralignbench_data/neuromod"
+)
+
+DATA_PATH_FORREST = (
+    "/data/parietal/store3/work/pbarbara/fmralignbench_data/forrest"
+)
+
 # path to memory
 MEMORY = "/data/parietal/store3/work/pbarbara/tmp"
