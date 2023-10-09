94cde56e2f9e80470fdb87cf1463d8a5c512a93b
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2023-10-09 23:57:03 +0200

    Fill test_config

--- test_config.py
+++ test_config.py
@@ -10,4 +10,17 @@ def check_test_solver_install(solver_class):
     particular architecture, call pytest.xfail when
     detecting the situation.
     """
-    pass
+    fmralign_solvers = [
+        'fastsrm',
+        'identity',
+        'ot',
+        'procrustes',
+        'ridge',
+    ]
+    
+    if solver_class.name.lower() in fmralign_solvers:
+        pytest.skip('fmralign solvers need to be installed from source')
+
+    if solver_class.name.lower() == 'fugw':
+        pytest.skip('Latest version of fugw is not available on PyPI')
+        
