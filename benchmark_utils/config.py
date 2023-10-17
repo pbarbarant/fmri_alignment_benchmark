ae1d9dabcdfa1c41e952495e88212dd3dc501a80
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2023-10-18 01:35:04 +0200

    Clean config

--- benchmark_utils/config.py
+++ benchmark_utils/config.py
@@ -1,20 +1,21 @@
-# path to IBC data
-DATA_PATH_IBC = "/data/parietal/store3/work/pbarbara/fmralignbench_data/ibc"
+from pathlib import Path
+
+root = "/data/parietal/store3/work/pbarbara/datasets/fmralign_benchopt_data"
+
+# path to IBC RSVP data
+DATA_PATH_IBC_RSVP = root / "IBC_RSVP"
+
+# path to IBC Sound data
+DATA_PATH_IBC_SOUND = root / "IBC_Sound"
+
+# path to IBC Mathlang data
+DATA_PATH_IBC_MATHLANG_AUDIO = root / "IBC_MathlangAudio"
 
 # path to Neuromod data
-DATA_PATH_NEUROMOD = (
-    "/data/parietal/store3/work/pbarbara/fmralignbench_data/neuromod"
-)
-
-# path to Forrest data
-DATA_PATH_FORREST = (
-    "/data/parietal/store3/work/pbarbara/fmralignbench_data/forrest"
-)
+DATA_PATH_NEUROMOD = root / "Neuromod"
 
 # path to BOLD5000 data
-DATA_PATH_BOLD5000 = (
-    "/data/parietal/store3/work/pbarbara/fmralignbench_data/BOLD5000"
-)
+DATA_PATH_BOLD5000 = root / "BOLD5000"
 
 # path to memory
 MEMORY = "/data/parietal/store3/work/pbarbara/tmp"
