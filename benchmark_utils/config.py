d91aee62e48bda46b12d287eaa305c0e1377815f
Denis Fouchard <denis.fouchard@inria.fr>
2023-12-12 16:20:36 +0100

    better config

--- benchmark_utils/config.py
+++ benchmark_utils/config.py
@@ -1,6 +1,15 @@
 from pathlib import Path
+from socket import gethostname
 
-root = Path("/storage/store2/work/dfouchar/fmralign_benchopt_data/")
+if gethostname().startswith("drago"):  # drago* machine
+    root = Path("/storage/store2/work/dfouchar/fmralign_benchopt_data/")
+    # path to memory
+    MEMORY = "/storage/store2/work/dfouchar/tmp"
+
+else:  # local machine
+    root = Path("/Users/df/datasets")
+    # path to memory
+    MEMORY = "/Users/df/tmp"
 
 # path to IBC RSVP data
 DATA_PATH_IBC_RSVP = root / "IBC_RSVP"
@@ -16,6 +25,3 @@ DATA_PATH_NEUROMOD = root / "Neuromod"
 
 # path to BOLD5000 data
 DATA_PATH_BOLD5000 = root / "BOLD5000"
-
-# path to memory
-MEMORY = "/storage/store2/work/dfouchar/tmp"
